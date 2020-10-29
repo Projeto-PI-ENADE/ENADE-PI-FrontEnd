00f69550a5e1842d28bb0ee40d2bab6fa7ec9c12
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-10-29 11:57:21 -0300

    Mudanças no ChartItem
    + Adds:
    1. Adição do TypeTitle;
    2. Adição do ToolTip nos botão de Mais Detalhes;
    3. Adição do ChartModal
    
    + Refactories:
    1.  Ajuste de estilização

--- src/components/chart/chart/styles.ts
+++ src/components/chart/chart/styles.ts
@@ -3,16 +3,20 @@ import { makeStyles, createStyles, Theme } from '@material-ui/core/styles';
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         container: {
+            justifyContent: 'center',
+            [theme.breakpoints.down('md')]: {
+                marginBottom: '1rem',
+            },
+        },
+
+        chartContentContainer: {
             padding: '1rem',
             border: '1px solid gray',
             borderRadius: '1rem',
             boxShadow: '2px 3px 6px #00000029',
-            [theme.breakpoints.down('md')]: {
-                marginBottom: '1rem',
-            },
 
-            '& h6': {
-                color: theme.palette.primary.dark,
+            [theme.breakpoints.down('md')]: {
+                padding: '.3rem',
             },
 
             '& button': {
@@ -24,6 +28,21 @@ const useStyles = makeStyles((theme: Theme) =>
                         width: 12,
                         height: 12,
                     },
+                },
+            },
+        },
+
+        titleContainer: {
+            '& h6': {
+                color: theme.palette.primary.dark,
+            },
+
+            [theme.breakpoints.down('md')]: {
+                paddingLeft: '1rem',
+                textAlign: 'left',
+
+                '& h6': {
+                    fontSize: '1rem',
                 },
             },
         },
