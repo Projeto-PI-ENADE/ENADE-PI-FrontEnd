834242a71acd096f525f80fafbac3ebd1f274abd
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-11-02 22:54:19 -0300

    Implementação do about/TechCard

---
+++ src/utils/data/techs.ts
@@ -0,0 +1,17 @@
+import NextJsLogo from '../../assets/techs/nextjs.png';
+import NodeJsLogo from '../../assets/techs/nodejs.png';
+import TypescriptLogo from '../../assets/techs/typescript.png';
+import MongoDbLogo from '../../assets/techs/mongodb.png';
+import JupyterLogo from '../../assets/techs/jupyter.png';
+import AdobeXDLogo from '../../assets/techs/adobexd.png';
+
+const techs = [
+    NextJsLogo,
+    NodeJsLogo,
+    TypescriptLogo,
+    MongoDbLogo,
+    JupyterLogo,
+    AdobeXDLogo,
+];
+
+export default techs;
