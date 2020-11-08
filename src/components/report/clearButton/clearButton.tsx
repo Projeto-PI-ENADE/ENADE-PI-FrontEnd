3a432843d36b5a91a20f2e7d8a17feace3246857
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-11-08 18:39:47 -0300

    Controle de IDs no ClearButton

--- src/components/report/clearButton/clearButton.tsx
+++ src/components/report/clearButton/clearButton.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import ButtonBase from '@material-ui/core/ButtonBase';
 import Typography from '@material-ui/core/Typography';
 import { Close as CloseIcon } from '@styled-icons/evaicons-solid';
@@ -8,11 +8,13 @@ import { Close as CloseIcon } from '@styled-icons/evaicons-solid';
 type Props = {
     initialData: Array<any>;
     setState: React.Dispatch<React.SetStateAction<any>>;
+    setIds: Dispatch<SetStateAction<Array<number>>>;
 };
 
-const ClearButton: React.FC<Props> = ({ initialData, setState }) => {
+const ClearButton: React.FC<Props> = ({ initialData, setState, setIds }) => {
     const clearBoxes = () => {
         setState(initialData);
+        setIds([]);
     };
 
     return (
