322fcd198e9fd2f85fb7a317277b87c670a62725
Jander Silva <42092038+JanderSilv@users.noreply.github.com>
2020-11-08 18:30:19 -0300

    Adição de ids nos dados

--- src/utils/data/report/scores.ts
+++ src/utils/data/report/scores.ts
@@ -1,26 +1,32 @@
 type ScoreType = {
+    id: number;
     label: string;
     checked: boolean;
 };
 
 const scores: Array<ScoreType> = [
     {
+        id: 0,
         label: 'Por quantidade total de alunos',
         checked: false,
     },
     {
+        id: 1,
         label: 'Por idade',
         checked: false,
     },
     {
+        id: 2,
         label: 'Por sexo',
         checked: false,
     },
     {
+        id: 3,
         label: 'Por renda familiar',
         checked: false,
     },
     {
+        id: 4,
         label: 'Modalidade de Ensino',
         checked: false,
     },
