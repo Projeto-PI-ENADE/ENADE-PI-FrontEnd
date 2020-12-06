c959ff25e61ab7c68f6db61fc8dfaead5dae16b5
Ruannilton <ruannilton@gmail.com>
2020-12-06 16:00:26 -0300

    antes da POG

--- src/pages/relatorio.tsx
+++ src/pages/relatorio.tsx
@@ -6,60 +6,141 @@ import TreeView from '@material-ui/lab/TreeView';
 import FormGroup from '@material-ui/core/FormGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import { ChevronRight, ChevronDown } from '@styled-icons/evaicons-solid';
+import coursesData from '../utils/data/courses_w_id_2018.json';
+import yearsData from '../utils/data/years';
+import { TypeCourses } from '../services/models/data';
+import ScrollToTopButton from '../components/scrollTopButton/scrollTopButton';
+import homeUseStyles from '../styles/pages/yearDashboard';
+import useStyles from '../styles/pages/report';
+import { Checkbox } from '@material-ui/core';
+import { TreeItem } from '@material-ui/lab';
+import Button from '@material-ui/core/Button';
+import { Plus } from '@styled-icons/entypo';
+
 
 import scoresData from '../utils/data/report/scores';
 import presencesData from '../utils/data/report/presences';
 import participantsData from '../utils/data/report/participants';
 import exportsData from '../utils/data/report/exports';
 import yearsReportData from '../utils/data/report/years';
-import coursesData from '../utils/data/courses_w_id_2018.json';
-import yearsData from '../utils/data/years';
-import { TypeCourses } from '../services/models/data';
 import { dataApi } from '../services/useCases/data';
-
 import ReportCard from '../components/report/reportCard';
 import RadioCard from '../components/report/radioCard';
 // import CoursesCard from '../components/report/coursesCard/coursesCard';
-import ScrollToTopButton from '../components/scrollTopButton/scrollTopButton';
-import homeUseStyles from '../styles/pages/yearDashboard';
-import useStyles from '../styles/pages/report';
-import { Checkbox } from '@material-ui/core';
-import { TreeItem } from '@material-ui/lab';
+
+let ids = 0;
+
+const getNewID = () => {
+    const tmp = ids;
+    ids++;
+    return String(tmp);
+}
+
+const FiltrosComponent: React.FC = () => {
+    const noID = getNewID()
+    const noNot = getNewID()
+    const noPre = getNewID()
+    const op = ['Por quantidade de alunos', 'Por idade', 'Por sexo', 'Por renda familiar', 'Por modalidade de ensino', 'Por etnia']
+    return (
+        <TreeItem nodeId={noID} label="Filtro">
+            <TreeItem nodeId={noNot} label="Nota">
+                <FormGroup row>
+                    {op.map(c => (
+                        <FormControlLabel
+                            id={getNewID()}
+                            control={<Checkbox />}
+                            label={c}
+                        />
+                    ))}
+                </FormGroup>
+            </TreeItem>
+            <TreeItem nodeId={noPre} label="Presença">
+                <FormGroup row>
+                    {op.map(c => (
+                        <FormControlLabel
+                            id={getNewID()}
+                            control={<Checkbox />}
+                            label={c}
+                        />
+                    ))}
+                </FormGroup>
+            </TreeItem>
+        </TreeItem>
+    )
+}
+
+const CursosComponent: React.FC<Array<number>> = (props: Array<number>) => {
+    const cursos = coursesData; //aplicar alguma logica de filtro aqui
+    const noID = getNewID()
+    console.log(props)
+    return (
+        <TreeItem nodeId={noID} label="Cursos">
+            <FormGroup row>
+                {cursos.map(c => (
+                    <FormControlLabel
+                        id={getNewID()}
+                        control={<Checkbox />}
+                        label={c.name}
+                    />
+                ))}
+            </FormGroup>
+            <FiltrosComponent />
+            <Button size="small" variant="contained"
+                color="default" endIcon={<Plus />} >
+                Adicionar Filtro
+            </Button>
+        </TreeItem>)
+}
+
+const AnosComponent: React.FC = () => {
+    const noID = getNewID()
+    return (
+        <TreeItem nodeId={noID} label="Anos">
+            <FormGroup row>
+                {yearsData.map(year => (
+                    <FormControlLabel
+                        id={getNewID()}
+                        control={<Checkbox />}
+                        label={year}
+                    />
+                ))}
+            </FormGroup>
+            <CursosComponent props={[0, 0, 0]} />
+            <Button variant="contained" size="small"
+                color="default" endIcon={<Plus />} >
+                Adicionar Curso
+            </Button>
+        </TreeItem>)
+}
+
+const ArquivoComponent: React.FC = () => {
+    const arqList = ['CSV', 'XLSX']
+    const noID = getNewID()
+
+    return (
+        <TreeItem nodeId={noID} label="Arquivo">
+            <FormGroup row>
+                {arqList.map(item => (
+                    <FormControlLabel
+                        id={getNewID()}
+                        control={<Checkbox />}
+                        label={item}
+                    />
+                ))}
+            </FormGroup>
+            <AnosComponent />
+            <Button variant="contained" size="small"
+                color="default" endIcon={<Plus />} >
+                Adicionar Ano
+            </Button>
+        </TreeItem>)
+}
 
 const Report: React.FC = () => {
     const homeClasses = homeUseStyles();
     const classes = useStyles();
-    let TreeID = 0;
-
     const [courses, setCourse] = useState<TypeCourses | null>(null);
 
-    const getTreeID = () => {
-        const tmp = TreeID;
-        TreeID++;
-        return String(tmp);
-    }
-
-    // const getCoursesData = async() => {
-    //     try {
-    //         const response = await dataApi.courses();
-
-    //     } catch (error) {
-    //         console.error(error);
-    //     }
-    // }
-
-    // const renderYearTree = () => {
-    //     return (
-    //         <FormGroup row>
-    //             { yearsReportData.map(year => (
-    //                 <FormControlLabel
-    //                     control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-    //                     label={year.year}
-    //                 />
-    //             ))}
-    //         </FormGroup>
-    //     )
-    // }
 
     return (
         <Grid container className={homeClasses.container}>
@@ -83,123 +164,11 @@ const Report: React.FC = () => {
                 <TreeView
                     defaultCollapseIcon={<ChevronDown />}
                     defaultExpandIcon={<ChevronRight />}>
-                    <TreeItem nodeId={getTreeID()} label="Relatorio">
-                        <TreeItem nodeId={getTreeID()} label="Arquivo">
-                            <FormGroup row>
-                                <FormControlLabel
-                                    control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                    label="CSV"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                    label="XLS"
-                                />
-                            </FormGroup>
-                            <TreeItem nodeId={getTreeID()} label="Anos">
-                                <FormGroup row>
-                                    {yearsData.map(year => (
-                                        <FormControlLabel
-                                            control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                            label={year}
-                                        />
-                                    ))}
-
-                                </FormGroup>
-                                <TreeItem nodeId={getTreeID()} label="Curos">
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Administração"
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Direito"
-                                    />
-                                </TreeItem>
-                            </TreeItem>
-
-
-                            <TreeItem nodeId={getTreeID()} label="Anos">
-                                <FormGroup row>
-                                    {yearsData.map(year => (
-                                        <FormControlLabel
-                                            control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                            label={year}
-                                        />
-                                    ))}
-
-                                </FormGroup>
-                                <TreeItem nodeId={getTreeID()} label="Curos">
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Administração"
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Direito"
-                                    />
-                                </TreeItem>
-                            </TreeItem>
-                        </TreeItem>
-                    </TreeItem>
-                    <TreeItem nodeId={getTreeID()} label="Relatorio">
-                        <TreeItem nodeId={getTreeID()} label="Arquivo">
-                            <FormGroup row>
-                                <FormControlLabel
-                                    control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                    label="CSV"
-                                />
-                                <FormControlLabel
-                                    control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                    label="XLS"
-                                />
-                            </FormGroup>
-                            <TreeItem nodeId={getTreeID()} label="Anos">
-                                <FormGroup row>
-                                    {yearsData.map(year => (
-                                        <FormControlLabel
-                                            control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                            label={year}
-                                        />
-                                    ))}
-
-                                </FormGroup>
-                                <TreeItem nodeId={getTreeID()} label="Curos">
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Administração"
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Direito"
-                                    />
-                                </TreeItem>
-                            </TreeItem>
-
-
-                            <TreeItem nodeId={getTreeID()} label="Anos">
-                                <FormGroup row>
-                                    {yearsData.map(year => (
-                                        <FormControlLabel
-                                            control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                            label={year}
-                                        />
-                                    ))}
-
-                                </FormGroup>
-                                <TreeItem nodeId={getTreeID()} label="Curos">
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Administração"
-                                    />
-                                    <FormControlLabel
-                                        control={<Checkbox /* checked={state.checkedA} onChange={handleChange} name="checkedA" */ />}
-                                        label="Direito"
-                                    />
-                                </TreeItem>
-                            </TreeItem>
-                        </TreeItem>
-                    </TreeItem>
-
+                    <ArquivoComponent />
+                    <Button variant="contained" size="small"
+                        color="default" endIcon={<Plus />} >
+                        Adicionar Arquivo
+                    </Button>
                 </TreeView>
             </Grid>
 
